c7c614eccc95bd41520fdfe47de5b3971b976b41
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2018-02-19 17:42:51 -0300

    Adiciona rota para a gerencia de projetos

--- app/api/index.js
+++ app/api/index.js
@@ -90,4 +90,87 @@ api.getUser = (req, res) => {
 	});
 };
 
+
+// projetos
+api.listProjects = (req, res) => {
+
+	const search = req.query;
+	const limit = (search.limit) ? parseInt(search.limit) : 10;
+	const skip = (search.page) ? parseInt(search.page) - 1 : 0;
+	const sort = { name: 1 };
+
+	delete search.page;
+	delete search.limit;
+
+	Object.keys(req.query).forEach(key => search[key] = new RegExp(req.query[key], 'i'));
+
+	db.find(search).skip(skip * limit).limit(limit).sort(sort).exec(function (err, doc) {
+		if (err)
+			res.status(500).json({ success: false, message: err });
+
+		res.json(doc);
+	});
+};
+
+api.insertProject = (req, res) => {
+	let project = req.body;
+
+	if (!project)
+		return;
+
+	delete project._id;
+	db.insert(project, function (err, newDoc) {
+		if (err)
+			res.status(500).json({ success: false, message: err });
+
+		console.log(`${newDoc._id} success written`);
+		res.json(newDoc._id);
+	});
+};
+
+api.updateProject = (req, res) => {
+	if (!req.params.identifier)
+		return res.json({ success: false, message: `parameter identifier can not be null` });
+
+	db.update({ _id: req.params.identifier }, req.body, function (err, numReplaced) {
+		if (err)
+			return res.json({ success: false, message: err });
+
+		if (numReplaced)
+			res.status(200).json({ success: true, message: `${req.params.identifier} success updated` });
+
+		res.status(500).end({ success: false, message: `can not find project ${req.params.identifier}` });
+	});
+};
+
+api.removeProject = (req, res) => {
+	if (!req.params.identifier)
+		return res.json({ success: false, message: `parameter identifier can not be null` });
+
+	db.remove({ _id: req.params.identifier }, { multi: false }, function (err, numRemoved) {
+		if (err)
+			res.status(500).json({ success: false, message: err });
+
+		if (numRemoved)
+			res.status(200).json({ success: true, message: `${req.params.identifier} success removed` });
+
+		res.status(500).json({ success: false, message: `can not find project ${req.params.identifier}` });
+	});
+};
+
+api.getProject = (req, res) => {
+	if (!req.params.identifier)
+		res.json({ success: false, message: `parameter identifier can not be null` });
+
+	db.findOne({ _id: req.params.identifier }, function (err, doc) {
+		if (err)
+			res.json({ success: false, message: err });
+
+		if (!doc)
+			res.json({ success: false, message: `Project can not be found. Maybe the identifier is wrong!` });
+
+		res.json(doc);
+	});
+};
+
 module.exports = api;
--- app/routes/index.js
+++ app/routes/index.js
@@ -15,6 +15,16 @@ module.exports = (app) => {
 		.get(api.getUser)
 		.put(api.updateUser);
 
+	// projetos
+	app.route('/v1/projects')
+		.post(api.insertProject)
+		.get(api.listProjects);
+
+	app.route('/v1/projects/:identifier')
+		.delete(api.removeProject)
+		.get(api.getProject)
+		.put(api.updateProject);
+
 	// not found
 	app.all('*', (req, res) => {
 		res.redirect('/v1');
